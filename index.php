8afe57075345f775db1f8e507b06b87ee56cae20
andreathepa <andreadepa32@gmail.com>
2023-07-10 14:11:33 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+
+    
+    
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+        <link rel="stylesheet" href="./style.css">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+        <title>Php OOP</title>
+    </head>
+    <body>
+        
+        <div class="wrapper">
+                <header>
+                    <div class="container mt-5 text-center">
+                        <h1>BOOLSHOP</h1>
+                    </div>
+                </header>
+                <div class="main">
+
+                </div>
+                <div class="container my-5">
+                    <div class="row">
+                        <div class="col-4"></div>
+                    </div>
+                </div>
+
+        </div>
+        <script type="text/javascript" src="./js/script.js"></script>
+
+    </body>
+</html>
